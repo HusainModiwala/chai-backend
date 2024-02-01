32390daea967f7e3148269e7bb7353bbefcdbc1c
HusainModiwala <hussainmodiwala@gmail.com>
2024-02-02 00:23:07 +0530

    Fixed isPublished method and implemented publish video method

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -41,6 +41,33 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if(!title || !description) throw new ApiError(400, "Please provide title and description to publish a video.");
+
+    const files = req.files;
+    const videoFilePath = files?.videoFile[0].path;
+    if(!videoFilePath) throw new ApiError(400, "No video file found. Please upload a video file to publish a video.");
+
+    const thumbnailPath = files?.thumbnail[0].path;
+    if(!thumbnailPath) throw new ApiError(400, "No thumbnail found. Please upload a thumbnail for the video.");
+
+    const videoResponse = await uploadOnCloudinary(videoFilePath);
+    const thumbnailResponse = await uploadOnCloudinary(thumbnailPath);
+    if(!videoResponse || !thumbnailResponse) throw new ApiError(500, "Error uploading fileson cloudinary.");
+
+    const video = await Video.create({
+        videoFile: videoResponse.url,
+        thumbnail: thumbnailResponse.url,
+        title: title,
+        description: description,
+        duration: videoResponse.duration,
+        views: 0,
+        isPublished: true,
+        owner: req.user._id
+    })
+    const finalVideoResponse = await Video.findById(video._id).select("-owner");
+    if(!video) throw new ApiError(500, "Failed to publish video.");
+
+    res.json(new ApiResponse(200, finalVideoResponse, "Video published successfully."));
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
@@ -75,17 +102,17 @@ const togglePublishStatus = asyncHandler(async (req, res) => {
     if(!videoId) throw new ApiError(400, "No videoId passed. Please pass a valid videoId.");
 
     // find video by id and update isPublished field
-    const video = await Video.findByIdAndUpdate(
-        videoId,
-        {$set: {isPublished: !isPublished }},
-        {new: true}
-    );
-
+    const video = await Video.findById(videoId);
     // if no video found throw error
     if(!video) throw new ApiError(404, "No video with given videoId found.");
+    const isPublished = video.isPublished;
+    video.isPublished = !isPublished;
+    await video.save({validateBeforeSave: false});
 
+    const response = await Video.findById(videoId);
+    console.log(response);
     // return success response
-    return res.json(200).json(new ApiResponse(200, video, "Video status toggled successfully"));
+    return res.status(200).json(new ApiResponse(200, response, "Video status toggled successfully"));
 })
 
 export {
